a62cb77bd387ab7c0942e461b1469edc10038fd0
Qwatrum <171450552+Qwatrum@users.noreply.github.com>
2026-05-13 16:57:46 +0200

    added files

---
+++ src/client/java/de/qwatrum/cobwebframe/client/CobwebframeClient.java
@@ -0,0 +1,13 @@
+package de.qwatrum.cobwebframe.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CobwebframeClient implements ClientModInitializer {
+
+
+
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
